5dafeb18e2d5c33eb7338aeb9e0c7aa6c55ab26f
Ellen Higgins ATU <114396639+g00413157@users.noreply.github.com>
2025-02-24 18:57:52 +0000

    added - header and index page

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="Logo/logo-2.png">
+    <title>Second Half Jerseys</title>
+    <link rel="stylesheet" href="styles.css"> 
+</head>
+<body>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+</style>
     <header class="header">
         <!-- Left navigation links -->
         <nav class="nav-links">
@@ -10,7 +22,7 @@
         <!-- Centered logo -->
         <div class="logo">
             <a href="index.php">
-                <img src="logo/logo-exported.png" alt="Logo">
+                <img src="logo/logo-4.png" alt="Logo">
             </a>
         </div>
 
@@ -21,3 +33,6 @@
         <button class="create" onclick="showSignUp()"> Create User</button>
     </nav>
     </header>
+    <h1>WHATS NEW</h1>
+</body>
+</html>
